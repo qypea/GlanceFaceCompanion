8cdad49ceacb48688e700aba5c9d5dec70559496
David Winn <q@qypea.com>
2016-07-16 09:31:00 -0700

    Fix string compare, which caused all calendar events to get ignored
    Why does == not work in Java? That's just dumb.

--- app/src/main/java/com/qypea/glancefacecompanion/CalendarScraper.java
+++ app/src/main/java/com/qypea/glancefacecompanion/CalendarScraper.java
@@ -73,7 +73,7 @@ public class CalendarScraper {
             // Determine if this is the one we want
             //noinspection StringEquality
             if (begin >= (CurrentTime - cooldownTime) // in the future or cooldown
-                && (allDay == "0") // not all day
+                && (allDay.equals("0")) // not all day
                 && (status == 1 || alarm == 1))  {// has alarm or accepted
 
                 // Get extra fields about it
