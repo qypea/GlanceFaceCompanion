be786addc05536fedaacc0c7cba6cc26c2238269
David Winn <q@qypea.com>
2016-08-10 19:36:16 -0700

    Start service on boot
    Fixes #3

---
+++ app/src/main/java/com/qypea/glancefacecompanion/StartupReceiver.java
@@ -0,0 +1,18 @@
+package com.qypea.glancefacecompanion;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by q on 8/10/16.
+ * Listen for startup indications, start service
+ */
+public class StartupReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent msg = new Intent(context, GlanceService.class);
+        msg.setType("boot");
+        context.startService(msg);
+    }
+}
